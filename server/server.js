87e9f6e9fe41ca2730407e6edd9fd094898a635e
asavant151 <asavant151@gmail.com>
2024-10-24 04:52:30 +0530

    changing in code

--- server/server.js
+++ server/server.js
@@ -6,16 +6,25 @@ import userRouter from './routes/userRoutes.js';
 
 // App Config
 const PORT = process.env.PORT || 4000;
-const app = express();
-await connectDB();
 
-// Intialize Middlewares
-app.use(express.json());
-app.use(cors());
+const startServer = async () => {
+  const app = express();
+  try {
+    await connectDB();
 
+    // Initialize Middlewares
+    app.use(express.json());
+    app.use(cors());
 
-// App routes
-app.get('/', (req,res) => res.send("API Working"));
-app.use('/api/user', userRouter)
+    // App routes
+    app.get('/', (req, res) => res.send("API Working"));
+    app.use('/api/user', userRouter);
 
-app.listen(PORT, () => console.log("Server Running on port " + PORT));
+    app.listen(PORT, () => console.log("Server Running on port " + PORT));
+  } catch (error) {
+    console.error("Error starting server:", error);
+    process.exit(1); // Exit the process if the server fails to start
+  }
+};
+
+startServer();
